1246a424ade69401e3dbcd177489fa5103752967
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2023-01-22 12:32:08 +0200

    added style.css

--- src/index.js
+++ src/index.js
@@ -20,10 +20,10 @@ function onInputChange({ target }) {
     fetchCountries(searchCountry)
       .then(country => {
         if (country.length < 2) {
-          createCountrieCard(country);
+          createCountryCard(country);
           Notify.success('Here your result');
         } else if (country.length < 10 && country.length > 1) {
-          createCountrieList(country);
+          createCountryList(country);
           Notify.success('Here your results');
         } else {
           reset();
@@ -39,33 +39,31 @@ function onInputChange({ target }) {
   }
 }
 
-function createCountrieCard(country) {
+function createCountryCard(country) {
   reset();
   const { flags, name, capital, population, languages } = country[0];
   const countryCard = `
-   <div class="country-card">
 		<div class="country-card__header">
 			<img class="country-card__img"src="${flags.svg}" alt="${
     name.official
   } width="55", height="35"">
-			<h2 class="country-card__name">${name.official}</h2>
+			<h2 class="country-card__title">${name.official}</h2>
 		</div>
-		<p class="country-card__text">Capital: <span>${capital}</span></p>
-		<p class="country-card__text">Population: <span>${population}</span></p>
-		<p class="country-card__text">Languages: <span>${Object.values(languages).join(
-      ','
-    )}</span></p>
-	</div>`;
+		<p class="country-card__text">Capital: <span class="country-card__value">${capital}</span></p>
+		<p class="country-card__text">Population: <span class="country-card__value">${population} people</span></p>
+		<p class="country-card__text">Languages: <span class="country-card__value">${Object.values(
+      languages
+    ).join(', ')}</span></p>`;
   countryInfo.innerHTML = countryCard;
 }
 
-function createCountrieList(array) {
+function createCountryList(array) {
   reset();
   listCountry.innerHTML = array
     .map(({ flags, name }) => {
-      `<li class="country-list__item">
-            <img src="${flags.svg}" alt="${name.official}" width="40", height="40">
-            <span class="country-list__name">${name.official}</span>
+      return `<li class="country-list__item">
+            <img class="country-list__img" src="${flags.svg}" alt="${name.official}" width="55", height="40">
+            <h3 class="country-list__title">${name.official}</h3>
         </li>`;
     })
     .join('');
